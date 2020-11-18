79b3a61d0f9f14397d5edb3c89a04caadd4ac965
erg <omn1present@tutanota.com>
2020-11-17 20:35:24 -0500

    Updated some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+
+}
+
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(11, 1);
+    serial_println!("[ok]");
+}
+
+
+mod vga_buffer;
+mod serial;
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -9,28 +33,38 @@ use core::panic::PanicInfo;
 // This gets called on panic.
 
 #[panic_handler]
-fn panic(_i: &PanicInfo) -> ! {
-	loop{
-		
-	}
+fn panic(i: &PanicInfo) -> ! {
+	println!("An error has occured: {}", i);
+    loop {}
 }
+
 
 
 // this prevents the name from being mangled.
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello, println{}", "!");
 
-	let vga_buffer = 0xb8000 as *mut u8;
+	#[cfg(test)]
+    test_main();
 
-	for (i, &byte) in HELLO.iter().enumerate() {
-		unsafe {
-			*vga_buffer.offset(i as isize * 2) = byte;
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-		}
-		
-	}
     loop {
-    	
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+	use x86_64::instructions::port::Port;
+	
+	unsafe {
+	    let mut port = Port::new(0xf4);
+	    port.write(exit_code as u32);
+	}
 }
